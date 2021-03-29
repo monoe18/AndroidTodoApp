dc0d52a36800ae0c3f251014567208855e0f4e77
Morten Breum <monoe18@student.sdu.dk>
2021-03-29 21:26:49 +0200

    Main Recycler View

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -4,15 +4,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var todoList = arrayListOf<ItemTodo>(
+            ItemTodo("Clean desk", false),
+            ItemTodo("Go Shopping", true),
+            ItemTodo("Cook Dinner", true)
+        )
+
+        val adapter = ListDisplayAdapter(todoList)
+        var recyclerView = findViewById<RecyclerView>(R.id.recyclerTodo)
+        recyclerView.setHasFixedSize(true)
+        var layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = adapter
     }
 
     fun newTodoNote(view: View) {
-        val intent = Intent(this, NewToDoNote::class.java).apply {        }
+        val intent = Intent(this, NewToDoNote::class.java).apply {}
         startActivity(intent)
     }
 }
