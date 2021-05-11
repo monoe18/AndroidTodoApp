d93d551be9e629b26fa8ac44ad95c8e0b903a3ec
Ahash Mathivathan <ahmat18@student.sdu.dk>
2021-05-11 19:21:03 +0200

    Update

--- app/src/main/java/com/example/todoapp/WidgetProvider.kt
+++ app/src/main/java/com/example/todoapp/WidgetProvider.kt
@@ -14,13 +14,13 @@ class WidgetProvider : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager?,
         appWidgetIds: IntArray?
     ) {
+
         super.onUpdate(context, appWidgetManager, appWidgetIds)
         if (appWidgetIds != null) {
             for (appWidgetId in appWidgetIds) {
                 val intent = Intent(context, MainActivity::class.java)
                 val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
                 val views = RemoteViews(context?.getPackageName(), R.layout.activity_widget)
-                views.setOnClickPendingIntent(R.id.example_widget_button, pendingIntent)
                 if (appWidgetManager != null) {
                     appWidgetManager.updateAppWidget(appWidgetId, views)
                 }
